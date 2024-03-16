9464aa1fc0b19a858805a3277450ecf55f93033a
Umesh Singadi <umeshsingadi007@gmail.com>
2024-03-16 21:37:58 +0530

    Create ExpandablePanel component

---
+++ src/components/ExpandablePanel.jsx
@@ -0,0 +1,23 @@
+import { useState } from "react";
+import { GoChevronDown, GoChevronLeft } from "react-icons/go";
+/* eslint-disable react/prop-types */
+
+function ExpandablePanel({ header, children }) {
+  const [expanded, setExpanded] = useState(false);
+
+  return (
+    <div className="mb-2 border rounded m-5">
+      <div className="flex p-2 justify-between items-center ">
+        <div className="flex flex-row items-center justify-between">
+          {header}
+        </div>
+        <div onClick={() => setExpanded(!expanded)} className="cursor-pointer">
+          {expanded ? <GoChevronDown /> : <GoChevronLeft />}
+        </div>
+      </div>
+      {expanded && <div className="p-2 border-t">{children}</div>}
+    </div>
+  );
+}
+
+export default ExpandablePanel;
--- src/components/UsersListItem.jsx
+++ src/components/UsersListItem.jsx
@@ -3,29 +3,31 @@ import Button from "./Button";
 import { deleteUser } from "../store";
 import { GoTrashcan } from "react-icons/go";
 import { useThunk } from "../hooks/useThunk";
-
+import ExpandablePanel from "./ExpandablePanel";
+import AlbumsList from "./AlbumsList";
 function UsersListItem({ user }) {
   const [doDeleteUser, isLoading, error] = useThunk(deleteUser);
 
   function handleDeleteUser(user) {
     doDeleteUser(user);
   }
+  const header = (
+    <>
+      <Button
+        className="mr-3"
+        loading={isLoading}
+        onClick={() => handleDeleteUser(user)}
+      >
+        <GoTrashcan />
+      </Button>
+      {error && <div>Error deleting user...</div>}
+      {user.name}
+    </>
+  );
   return (
-    <div className="mb-2 border rounded m-5">
-      <div className="flex p-2 justify-between items-center cursor-pointer">
-        <div className="flex flex-row items-center justify-between">
-          <Button
-            className="mr-3"
-            loading={isLoading}
-            onClick={() => handleDeleteUser(user)}
-          >
-            <GoTrashcan />
-          </Button>
-          {error && <div>Error deleting user...</div>}
-          {user.name}
-        </div>
-      </div>
-    </div>
+    <ExpandablePanel header={header}>
+      <AlbumsList user={user}></AlbumsList>
+    </ExpandablePanel>
   );
 }
 
