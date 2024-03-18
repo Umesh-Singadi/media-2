6cc5ff21eca1127a73c3ebfde13151098ab9da47
Umesh Singadi <umeshsingadi007@gmail.com>
2024-03-18 21:08:38 +0530

    Create photosApi

--- src/components/AlbumsListItem.jsx
+++ src/components/AlbumsListItem.jsx
@@ -3,7 +3,7 @@ import ExpandablePanel from "./ExpandablePanel";
 import { GoTrashcan } from "react-icons/go";
 import Button from "./Button";
 import { useRemoveAlbumMutation } from "../store";
-
+import PhotosList from "./PhotosList";
 function AlbumsListItem({ album }) {
   const [removeAlbum, results] = useRemoveAlbumMutation();
   function handleClick() {
@@ -22,8 +22,8 @@ function AlbumsListItem({ album }) {
     </div>
   );
   return (
-    <ExpandablePanel key={album.id} header={header}>
-      <div>{album.title} Album Photos</div>
+    <ExpandablePanel header={header}>
+      <PhotosList album={album}></PhotosList>
     </ExpandablePanel>
   );
 }
